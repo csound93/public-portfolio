81b101c0b6cc87221cf50673b149faeece0516c7
PyongkiChoi <csound93@gmail.com>
2025-01-23 23:13:44 +0900

    fix: 코드블럭의 색상 중 글자 색상만 변하지 않는 오류를 수정한다
    - 공식 기능에도 bg색상 외에 별도로 글자색 기능을 제공하지 않는 것으로 보여, background 색상을 고정해서 처리하도록 디자인을 변경한다

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,59 @@
 import createMDX from '@next/mdx';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
+import remarkGfm from 'remark-gfm';
+import rehypePrettyCode from 'rehype-pretty-code';
+import type { NextConfig } from 'next';
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+interface Node {
+  children: Array<{ type: string; value: string }>;
+  properties?: {
+    className: string[];
+  };
+}
+
+const prettyCodeOptions = {
+  keepBackground: true,
+  grid: true,
+  themes: {
+    light: 'github-light',
+    dark: 'github-dark'
+  },
+  onVisitLine(node: Node) {
+    if (node.children.length === 0) {
+      node.children = [{ type: 'text', value: ' ' }];
+    }
+  },
+  onVisitHighlightedLine(node: Node) {
+    if (node.properties) {
+      node.properties.className.push('highlighted');
+    }
+  },
+  onVisitHighlightedWord(node: Node) {
+    if (node.properties) {
+      node.properties.className = ['word'];
+    }
+  },
+};
+
+const nextConfig: NextConfig = {
   // Configure `pageExtensions` to include markdown and MDX files
-  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
+  webpack: (config) => {
+    config.module?.rules?.push({
+      test: /\.mmd$/,
+      type: 'asset/source',
+    });
+    return config;
+  },
   // Optionally, add any other Next.js config below
 };
 
 const withMDX = createMDX({
-  // Add markdown plugins here, as desired
+  options: {
+    remarkPlugins: [remarkMath, remarkGfm],
+    rehypePlugins: [rehypeKatex, [rehypePrettyCode, prettyCodeOptions]],
+  },
 });
 
 // Merge MDX config with Next.js config
